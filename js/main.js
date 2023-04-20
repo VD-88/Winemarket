ce4f861f883b281c63e20fc59c8478433955b894
Yaroslav Tkachyk <purity.xd@gmail.com>
2023-04-20 15:39:52 +0000

    setup carousel

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          speed: 1500,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: true,
+          infinite: true,
+          dots: true,
+          speed: 1500,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      }
+    ]
+  });
 });
