75e07c6fa5ab55e1c812f50cd31d3f685feb65ae
maksym-shulha <123635020+maksym-shulha@users.noreply.github.com>
2023-04-27 11:34:42 +0300

    add product list

---
+++ js/craft-wines.js
@@ -0,0 +1,3 @@
+// function renderRedWines(products) {
+//     const productList = document.querySelector('.')
+// }
---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("partialsLoaded", () => {
+    import('./product-list.js');
+    import('./craft-wines.js');
+});
