25bcc3e91ba7a985489e8a7d1c04e4e1b84aed37
Yaroslav Tkachyk <purity.xd@gmail.com>
2023-04-30 06:37:18 +0000

    import all .js to index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 document.addEventListener("partialsLoaded", () => {
+    import ('https://code.jquery.com/jquery-3.6.4.min.js');
+    import ('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js');
+    import ('../slick/slick.min.js');
+    import ('./carousel.js');
     import('./product-list.js');
     import('./craft-wines.js');
 });
